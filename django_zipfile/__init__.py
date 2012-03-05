088037526e39a868af1e2045fa5e158872c92817
Flavio Curella <flavio.curella@gmail.com>
2012-03-05 14:18:44 -0600

    multiple templates per-file

--- django_zipfile/__init__.py
+++ django_zipfile/__init__.py
@@ -4,7 +4,7 @@ import sys
 import os
 from zipfile import ZipFile
 from django.template import Context
-from django.template.loader import get_template
+from django.template.loader import render_to_string
 
 
 class TemplateZipFile(ZipFile, object):
@@ -39,24 +39,36 @@ class TemplateZipFile(ZipFile, object):
             if compress_type != self.compress_type and sys.version_info < (2, 7):
                 raise "Python2.7 is required for individual file compression."
 
-    def add_template(self, template_name, filename=None, context=None, compress_type=None):
+    def _template_name(self, template):
+        if self.template_root is not None:
+            return self.template_root + template
+        return template
+
+    def add_template(self, template_list, filename=None, context=None, compress_type=None):
         self._check_individual_compression_supported(compress_type)
         if context is None:
             c = Context({})
         else:
             c = Context(context)
 
-        if self.template_root is not None:
-            template_name = self.template_root + template_name
+        templates = []
 
-        template = get_template(template_name)
-        render = template.render(c)
+        if isinstance(template_list, basestring):
+            templates = [self._template_name(template_list)]
+        else:
+            for template in template_list:
+                templates.append(self._template_name(template))
+
+        render = render_to_string(templates, c)
 
         if filename is None:
+            template_name = templates[0]
+
             if self.template_root is not None:
                 filename = template_name.split(self.template_root)[1]
             else:
                 filename = template_name.split('/')[-1]
+
         if compress_type is not None:
             self.writestr(filename, render, compress_type)
         else:
