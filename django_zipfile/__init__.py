917818a6093500cdf9418db654c15cf8ce8e3ee5
Flavio Curella <flavio.curella@gmail.com>
2013-05-20 10:47:59 -0500

    fixed setup.py

--- django_zipfile/__init__.py
+++ django_zipfile/__init__.py
@@ -1,14 +1,2 @@
-VERSION = (0, 1, 7)
 
-# This tries to import TemplateZipFile only when django is installed.
-# For example, when installing via pip, setup.py executes this file before
-# installing Django, even if that's specified as dependency.
-try:
-    import django
-    django_present = True
-except ImportError:
-    django_present = False
-
-# We still want to catch eventual ImportErrors that are due to TemplateZipFile.
-if django_present:
-    from django_zipfile.templatezipfile import TemplateZipFile
+from django_zipfile.templatezipfile import TemplateZipFile
